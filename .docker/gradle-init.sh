b3ac0c0cebdaf2b6ffdf5156ce5bf8989b546bad
Ismail Moukafih <i.moukafih@gmail.com>
2024-06-08 10:32:19 +0200

    Update gradle-init.sh
    update gradle init script

--- .docker/gradle-init.sh
+++ .docker/gradle-init.sh
@@ -2,5 +2,4 @@
 echo "-------------------------------------------------------------------"
 echo "-                create project with gradle                       -"
 echo "-------------------------------------------------------------------"
-gradle init   --type kotlin-application   --dsl kotlin   --test-framework junit-jupiter   --package nl.moukafih   --project-name my-project    --no-split-project    --java-version 17  <<< 'no'
-  
+gradle init --type kotlin-application --dsl kotlin --test-framework junit-jupiter --package nl.moukafih --project-name my-project --no-split-project --java-version 17 <<< 'no'
